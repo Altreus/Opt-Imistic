334b310630cf723bdd12da5ec733d7e161eb7c21
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2014-06-10 14:10:43 +0100

    Documented the hints I implemented ages ago

--- lib/Opt/Imistic.pm
+++ lib/Opt/Imistic.pm
@@ -164,7 +164,7 @@ Opt::Imistic - Optimistic option parsing
     use Opt::Imistic;
     die if $ARGV{exit};
 
-=head1 
+=head1
 
 =head1 DESCRIPTION
 
@@ -174,7 +174,8 @@ spec to do it. This one just gets all the options and then gets out of your way.
 For the most part, your command-line options will probably be one of two things:
 a toggle (or maybe a counter), or a key/value pair. Opt::Imistic assumes this
 and parses your options. If you need more control over it, Opt::Imistic is not
-for you and you might want to try a module such as L<Getopt::Long>.
+for you and you might want to try a module such as L<Getopt::Long>. That being
+said, see the L<CONFIGURATION> section for ways to configure Opt::Imistic.
 
 The hash C<%ARGV> contains your arguments. The argument name is provided as the
 key and the value is provided as the value. If you use the same argument
@@ -267,7 +268,7 @@ Z<>
     @ARGV = ('foo.pl')
 
 Z<>
-    
+
     script.pl -foo
 
     f => [ 1 ]
@@ -279,20 +280,148 @@ Z<>
 
     f => [ 1 ]
     o => [undef, 'bar']
-    
+
 Z<>
 
     script.pl --foo bar --foo=bar
 
     foo => ['bar', 'bar']
 
-Z<> 
+Z<>
+
+=head1 CONFIGURATION
+
+Opt::Imistic can be crudely configured to deal with certain options specially.
+Configuration is done by means of the import list, meaning all requirements
+will have been satisfied before the program even finishes compiling.
+
+=head2 Putback
+
+If the first item in your import list is an integer, this will be used as the
+putback value. Observe that this will be C<shift>ed from the C<import> list,
+and the rest of the list will be used to construct a hash.
+
+Putback refers to the act of putting arguments back onto C<@ARGV>. This allows
+your script to be called with non-value options which are followed by actual
+arguments:
+
+    $ push.pl -v master
+
+    # %ARGV
+    {
+      'v' => bless( [
+                      'master'
+                    ], 'Opt::Imistic::Option' )
+    };
+
+
+If C<push.pl> is an Opt::Imistic script, C<$ARGV{v}> would have the value
+C<master>, and C<@ARGV> would not have anything on it at all.
+
+To fix this, you tell Opt::Imistic that you need at least one argument:
+
+    use Opt::Imistic (1);
+
+Now, C<@ARGV> will always have at least one (non-option) entry. If this cannot
+be satisfied, the script bails.
+
+    $ push.pl -v master
+
+    # %ARGV:
+    {
+      '-' => bless( [
+                      'master'
+                    ], 'Opt::Imistic::Option' ),
+      'v' => [
+               1,
+             ]
+    };
+
+Note that C<$ARGV{'-'}> is a copy of the remaining C<@ARGV>.
+
+The putback argument will be observed I<before> the C<demand> or C<needs_val>
+options. This means that if you provide an option that needs a value, and not
+enough arguments to the script itself, the script arguments will be checked
+before the option's argument:
+
+    use Opt::Imistic (1, needs_val => {remote => 1});
+
+    $ push.pl --remote master
+    remote: value required but none given
+
+In this example, C<master> was used as the putback argument, leaving
+C<--remote> with no value.
+
+    use Opt::Imistic (2, needs_val => {remote => 1});
+
+    $ push.pl --remote master
+    Expected 2 arguments; 1 given
+
+This is a bit clearer: putback wanted 2 arguments, so the script bailed before
+C<--remote> even got a chance to look for arguments.
+
+=head2 Required options
+
+Sometimes your option is required. This is the case when it needs a value. It
+doesn't make sense for an option to be required with no value, unless you
+really want to make sure someone means it before they run your script. In that
+case, you can check C<%ARGV> yourself.
+
+Opt::Imistic will bail if any of the keys of the C<demand> hashref are missing.
+It will also bail if the option is given without a value.
+
+C<demand> is provided as a hashref simply to make it easier to look up items.
+
+    use Opt::Imistic (demand => { branch => 1 });
+
+This invocation means that C<--branch> must be given and have a value:
+
+    $ push.pl
+    Missing option: branch
+    BEGIN failed--compilation aborted at push.pl line 4
+
+    $ push.pl --branch
+    branch: value required but none given
+    BEGIN failed--compilation aborted at push.pl line 4.
+
+=head2 Required values
+
+Some options are, well, optional, but don't make sense without a value. For
+this, use the needs_val hashref:
+
+    use Opt::Imistic (needs_val => { branch => 1 });
+
+Now if C<--branch> is given it must have a value, but if not, that's also fine:
+
+    $ push.pl
+
+    $ push.pl --branch
+    branch: value required but none given
+    BEGIN failed--compilation aborted at push.pl line 4.
+
+=head2 Usage
+
+If you provide a C<usage> key in the hash-like options list, it will be printed
+with anything that causes Opt::Imistic to die.
+
+    use Opt::Imistic (2,
+        needs_val => { remote => 1 },
+        usage => "Usage: push.pl [--remote=<remote>] branch\n"
+    );
+
+    Expected 2 arguments; 1 given
+
+    Usage: push.pl [--remote=<remote>] branch
 
 =head1 BUGS AND TODOS
 
-It should be noted that Opt::Imistic does not observe a difference between C<-f> and C<--f>.
+It should be noted that Opt::Imistic does not observe a difference between
+C<-f> and C<--f>.
 
-No known bugs.
+Counted options lose their blessing as Opt::Imistic::Option, which may
+sometimes break code that didn't expect it to be a counted option.
+
+Usage messages cause the die line to be reported if they don't have their own newline.
 
 Please report undesirable behaviour, but note the TODO list first:
 
@@ -303,7 +432,7 @@ delimiting from their values
 
 =item Implement further hints to alias options.
 
-=item Implement a required list and a usage string
+=item Allow usage to be a coderef.
 
 =back
 
